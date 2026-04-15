ca029e0405eb31b252d686f3b45837ef63b88810
Alwin Doss <alwindoss84@gmail.com>
2026-04-15 12:47:04 +0530

    working application with tailwindcss navbar
    Signed-off-by: Alwin Doss <alwindoss84@gmail.com>

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue';
+</script>
+
+<template>
+<div class="container">
+  <Navbar app-name="Ducky"/>
+</div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #e80000aa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ frontend/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import tailwindcss from '@tailwindcss/vite'
+import vue from "@vitejs/plugin-vue";
+import wails from "@wailsio/runtime/plugins/vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    tailwindcss(),
+    wails("./bindings"),
+  ],
+});
